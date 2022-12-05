95cea1dd3d6d2c2390e86ce3b67382965b6f17ab
nikkiwu <nikki851205@gmail.com>
2022-12-05 16:25:57 +0800

    npm init vue 建環境
    npm i 建資料夾 , npm run dev 跑出網址(這網址才能看到.類似自動更新網頁，npm run build 打包 (這的檔案就能用自動更新網頁

---
+++ 20221205vue/vue-project/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>卡片</h1>
+  <p>總共 {{ totalGood }} 個讚</p>
+  <aCard v-for="(card, i) in cards" :key="i" :img="card.img" :text="card.text" v-model:good="card.good"></aCard>
+</template>
+
+
+<!-- <script>
+import { reactive, computed } from 'vue';
+import aCard from './components/aCard.vue'
+export default {
+  components: { aCard },
+  setup() {
+    const cards = reactive([{ img: 'https://github.com/rogeraabbccdd.png', text: 'rogeraabbccdd', good: true },
+    { img: 'https://github.com/vuejs.png', text: 'Vue.js', good: true },
+    { img: 'https://github.com/vitejs.png', text: 'Vite.js', good: true }])
+    const totalGood = computed(() => {
+      return cards.filter(card => card.good).length
+    })
+    return { cards, totalGood }
+  }
+
+}
+</script> -->
+
+<!-- 更簡單的 -->
+<script setup>
+import { reactive, computed } from 'vue'
+import aCard from './components/aCard.vue'
+
+const cards = reactive([{ img: 'https://github.com/rogeraabbccdd.png', text: 'rogeraabbccdd', good: true },
+{ img: 'https://github.com/vuejs.png', text: 'Vue.js', good: true },
+{ img: 'https://github.com/vitejs.png', text: 'Vite.js', good: true }])
+const totalGood = computed(() => {
+  return cards.filter(card => card.good).length
+})
+</script>
+<style>
+p {
+  color: brown;
+}
+</style>
+
+<style scoped>
+p {
+  color: rgb(25, 49, 207);
+}
+</style>
---
+++ 20221205vue/vue-project/src/components/aCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card">
+    <img :src="img">
+    <p>{{ text }}</p>
+    <input type="button" :value="g ? '收回讚' : '讚'" @click="(g = !g)">
+  </div>
+</template>
+
+<!-- <script>
+import { computed } from 'vue'
+export default {
+  props: ['img', 'text', 'good'],
+  setup (props, { emit }) {
+    const g = computed({
+      get () {
+        return props.good
+      },
+      set (value) {
+        emit('update:good', value)
+      }
+    })
+    return { g }
+  }
+}
+</script> -->
+
+<script setup>
+import { computed, defineProps, defineEmits } from 'vue'
+
+const props = defineProps(['img', 'text', 'good'])
+const emit = defineEmits(['update:good'])
+
+const g = computed({
+  get() {
+    return props.good
+  },
+  set(value) {
+    emit('update:good', value)
+  }
+})
+</script>
