2383bc8f9072800fc0bc9efc0df64a60c6883a75
nikkiwu <nikki851205@gmail.com>
2022-12-26 15:31:39 +0800

    增加env

--- 20221226/index.js
+++ 20221226/index.js
@@ -14,3 +14,12 @@ app.use('/users', userRoute)
 app.listen(process.env.PORT || 4000, () => {
   console.log('伺服器啟動')
 })
+
+// env
+// DB_URL=mongodb+srv://nikki851205:Nn0922335584@cluster0.nvicjnu.mongodb.net/20221226
+
+// JWT_SECRET=20221226
+
+// CLOUDINARY_NAME=dgsdfnhhp
+// CLOUDINARY_KEY=847626483916576
+// CLOUDINARY_SECRET=AzKkyrttcvvOsnBoj5H0udoimEg
