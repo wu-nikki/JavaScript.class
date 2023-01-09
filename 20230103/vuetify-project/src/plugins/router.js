0c7abca4dad422e3624c695c627dfd2355ff98fa
nikkiwu <nikki851205@gmail.com>
2023-01-09 16:24:12 +0800

    設定攔截亂入頁面的人

--- 20230103/vuetify-project/src/plugins/router.js
+++ 20230103/vuetify-project/src/plugins/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import FrontLayout from '@/layouts/FrontLayout.vue'
 import FrontHomeView from '@/views/front/HomeView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import { useUserStore } from '@/stores/user'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.BASE_URL),
@@ -15,7 +16,9 @@ const router = createRouter({
           name: 'home',
           component: FrontHomeView,
           meta: {
-            title: '購物網'
+            title: '購物網',
+            login: false,
+            admin: false
           }
         },
         {
@@ -23,7 +26,9 @@ const router = createRouter({
           name: 'register',
           component: () => import('@/views/front/RegisterView.vue'),
           meta: {
-            title: '購物網|註冊'
+            title: '購物網|註冊',
+            login: false,
+            admin: false
           }
         },
         {
@@ -31,7 +36,9 @@ const router = createRouter({
           name: 'login',
           component: () => import('@/views/front/LoginView.vue'),
           meta: {
-            title: '購物網|註冊'
+            title: '購物網|登入',
+            login: false,
+            admin: false
           }
         }
       ]
@@ -46,7 +53,9 @@ const router = createRouter({
           name: 'admin-home',
           component: () => import('@/views/admin/HomeView.vue'),
           meta: {
-            title: '購物網 | 管理'
+            title: '購物網 | 管理',
+            login: true,
+            admin: true
           }
         }
       ]
@@ -57,7 +66,9 @@ const router = createRouter({
       name: '404',
       component: NotFoundView,
       meta: {
-        title: '購物網|404'
+        title: '購物網|404',
+        login: false,
+        admin: false
       }
     }, {
       path: '/:pathMatch(.*)*',
@@ -69,6 +80,29 @@ const router = createRouter({
 
 router.afterEach((to, from) => {
   document.title = to.meta.title
+})
+// 阻擋亂入
+router.beforeEach(async (to, from, next) => {
+  console.log('beforeEach')
+  const user = useUserStore()
+  // 登入後，如果又點了註冊或是登入 把他導回首頁
+  if (user.isLogin && (to.path === '/register' || to.path === '/login')) {
+    next('/')
+    // 如果去要登入的頁面 但他沒登入 ，把他導去登入頁
+  } else if (to.meta.login && !user.isLogin) {
+    next('/login')
+    // 如果去要管理的頁面 ，要先判斷是不是管理員
+  } else if (to.meta.admin) {
+    await user.getUser()
+    // 如果不是就回首頁
+    if (!user.isAdmin) {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
