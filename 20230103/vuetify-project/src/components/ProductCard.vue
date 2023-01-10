f745ee916b7a6f00ea346d6e24630f35a676326e
nikkiwu <nikki851205@gmail.com>
2023-01-10 16:19:10 +0800

    把商品顯示在首頁

---
+++ 20230103/vuetify-project/src/components/ProductCard.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+v-card.card-product
+    v-img(:src="image" cover height="200px")
+    v-card-title {{ name }}
+    v-card-subtitle ${{ price }}
+    v-card-text {{ description }}
+    v-card-actions
+      v-btn(color="primary" prepend-icon="mdi-cart") 加入購物車
+</template>
+<script setup>
+defineProps(
+  {
+    /* eslint-disable */
+    _id: {
+      type: String,
+      default: ''
+    },
+     /* eslint-enable */
+    name: {
+      type: String,
+      default: ''
+    },
+    price: {
+      type: Number,
+      default: 0
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    image: {
+      type: String,
+      default: ''
+    },
+    sell: {
+      type: Boolean,
+      default: false
+    },
+    category: {
+      type: String,
+      default: ''
+    }
+  }
+)
+</script>
--- 20230103/vuetify-project/src/views/front/HomeView.vue
+++ 20230103/vuetify-project/src/views/front/HomeView.vue
@@ -1,4 +1,32 @@
 <template lang="pug">
 #home
-  h1.text-center 首頁
+  v-row
+    v-col(cols="12")
+      h1.text-center 購物網
+    v-divider
+    v-col(v-for="product in products" :key="product._id" cols="12" md="6" lg="3")
+      ProductCard(v-bind="product")
+
 </template>
+<script setup>
+import { reactive } from 'vue'
+import { api } from '@/plugins/axios'
+import Swal from 'sweetalert2'
+import ProductCard from '@/components/ProductCard'
+
+const products = reactive([]);
+
+(async () => {
+  try {
+    const { data } = await api.get('/products')
+    products.push(...data.result)
+  } catch (error) {
+    Swal.fire({
+      icon: 'error',
+      title: '失敗',
+      text: error?.response?.data?.message || '發生錯誤'
+    })
+  }
+})()
+
+</script>
